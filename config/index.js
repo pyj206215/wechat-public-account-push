81d3a8c1b73a1e92a38fc7c067a5a0b910529335
pyj206215 <114185012+pyj206215@users.noreply.github.com>
2022-09-23 15:42:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "wx82bed38899d64c94",
+    appId: "555d62784e765b8f2f968a9ab7754205",
     // 公众号appSecret
     appSecret: "68352c3e8a8826d00fdbc3655b58ed36",
     // 模板消息id
-    templateId: "chPHTZXoYmAR0_lyPrZMhwanOc3JbkJMqs_TOw0E_Fk",
+    templateId: " BfUIz7lbBF9oFYaxs8y2c2ehQexlP_T8AcghPSr8Q1s",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [oGh_L52iTgjQ4krxdCL-e_egahrQ],
+    user: [ oOlmC6vR3AdzexzsmSteTizWxKTU],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "重庆",
     // 所在城市
-    city: "惠州",
+    city: "九龙坡",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
